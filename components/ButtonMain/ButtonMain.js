e79f967f9794647b7bf18b266ba85911bb9068eb
ZGennadiy <zg@2glab.com>
2020-11-06 13:53:15 +0300

    addd opacity to button

--- components/ButtonMain/ButtonMain.js
+++ components/ButtonMain/ButtonMain.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import React from 'react'
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    button: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'stretch',
-        height: 50,
-        borderRadius: 4,
-        backgroundColor: '#ffffff',
-        opacity: 0.5,
-    },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    height: 50,
+    borderRadius: 4,
+    backgroundColor: '#ffffff',
+    opacity: 0.5,
+  },
 
-    buttonText: {
-        fontSize: 15,
-        textTransform: 'uppercase',
-        color: '#3B84BE',
-    },
-});
+  buttonText: {
+    fontSize: 15,
+    textTransform: 'uppercase',
+    color: '#3B84BE',
+  },
+})
 
-export default function ButtonMain({onPress, disableButton}) {
-
-    return (
-        <TouchableOpacity style={[styles.button, {opacity: disableButton ? 1 : 1}]} disabled={disableButton} onPress={onPress}>
-            <Text style={styles.buttonText}>
-                начать
-            </Text>
-        </TouchableOpacity>
-    );
+export default function ButtonMain({ onPress, disableButton }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { opacity: disableButton ? 1 : 1 }]}
+      disabled={disableButton}
+      onPress={onPress}
+    >
+      <Text style={styles.buttonText}>начать</Text>
+    </TouchableOpacity>
+  )
 }
