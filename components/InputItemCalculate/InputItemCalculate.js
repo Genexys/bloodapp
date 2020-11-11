81a1d9875d9ac67e320caa5e9db0e39abaca3acb
ZGennadiy <zg@2glab.com>
2020-11-11 12:24:36 +0300

    went back placeholder - 0 on searchCalculate page

--- components/InputItemCalculate/InputItemCalculate.js
+++ components/InputItemCalculate/InputItemCalculate.js
@@ -55,7 +55,7 @@ export default function InputItemCalculate({ short, long, listOfValues, getValid
       <View style={[styles.inputContainer, { borderBottomColor: item.value === '' ? '#3B84BE' : '#014F80' }]}>
         <TextInput
           placeholderTextColor={item.value === '' ? '#3B84BE' : '#014F80'}
-          placeholder={'-'}
+          placeholder={'0'}
           style={styles.input}
           keyboardType={'numeric'}
           value={item.value}
